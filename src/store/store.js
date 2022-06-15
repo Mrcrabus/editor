a5f7b462597d7f421345bf51546e1bc3f21ebf1f
Igor <42350880+Mrcrabus@users.noreply.github.com>
2022-06-15 09:45:59 +0300

    LocalStorage

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,25 @@
 import { configureStore } from '@reduxjs/toolkit'
 import tagsSlice from "./tagsSlice";
 
+const localStorageMiddleware = ({getState}) => {
+  return next => action => {
+    const result = next(action);
+    localStorage.setItem('notes', JSON.stringify(getState()));
+    return result;
+  };
+};
+
+const reHydrateStore = () => {
+  if(localStorage.getItem('notes') !== null) {
+    return JSON.parse(localStorage.getItem('notes'));
+  }
+};
+
 export const store = configureStore({
   reducer: {
     tags: tagsSlice,
   },
-})
+  preloadedState: reHydrateStore(),
+  middleware: getDefaultMiddleware =>
+    getDefaultMiddleware().concat(localStorageMiddleware),
+});
